e97e74e6d6ec80db632e5014550cfdcbfc417f4f
Ismail Magdy <ismailmagdy566@gmail.com>
2023-03-01 19:51:14 +0200

    Angular Project

---
+++ src/app/components/Auth/User/registar-user/registar-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registar-user',
+  templateUrl: './registar-user.component.html',
+  styleUrls: ['./registar-user.component.css']
+})
+export class RegistarUserComponent {
+
+}
