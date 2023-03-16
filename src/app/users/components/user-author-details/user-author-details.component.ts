254f22d608a8ccc82d41a0c940df892a5dc56c0a
OmarGamal07 <omar.gamal.dev@gmail.com>
2023-03-16 17:36:18 +0200

    editting author

--- src/app/users/components/user-author-details/user-author-details.component.ts
+++ src/app/users/components/user-author-details/user-author-details.component.ts
@@ -8,13 +8,13 @@ import { AuthorService } from 'src/app/services/user/author.service';
   styleUrls: ['./user-author-details.component.css']
 })
 export class UserAuthorDetailsComponent implements OnInit,OnChanges{
-  authors!: any
+  author!: any
   autherID !: number
   constructor (private authorService : AuthorService,
                 private ActivatedRoute : ActivatedRoute){
                   this.ActivatedRoute.paramMap.subscribe((paramMap)=>{
                     //convert ID to int and check to null
-                    this.autherID =  +paramMap.get('ID')! || 1  ;
+                    this.autherID =  +paramMap.get('id')! || 1  ;
                   })
                  }
   ngOnChanges() {
@@ -22,8 +22,8 @@ export class UserAuthorDetailsComponent implements OnInit,OnChanges{
   }
 
   ngOnInit() {
-    this.authorService.getAllAuthors()
-    .subscribe((aut) => this.authors = aut);
+    this.authorService.getAuthorByID(this.autherID)
+    .subscribe((aut) => this.author = aut);
     
   }
 }
