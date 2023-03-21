980f6f2fd16e12ea44bd99b67af4f45029723df7
OmarGamal07 <omar.gamal.dev@gmail.com>
2023-03-22 00:28:19 +0200

    pull  news

--- src/app/@core/api.service.ts
+++ src/app/@core/api.service.ts
@@ -21,20 +21,20 @@ export class ApiService {
 
   }
 
-
   post(url:string,body:any):Observable<any>
   {
     const headers = new HttpHeaders({
-        'Content-Type': 'multipart/form-data',
+      
+        'Content-Type': 'multipart/form-data; boundary=my',
          Accept: '*/*',
         'Access-Control-Allow-Origin': '*',
 
     });
-
-    return this.httpClient.post(`${url}`,JSON.stringify(body),
-    {
-      headers,
-    });
+    const options = {
+      headers: headers
+    };
+     return this.httpClient.post(`${url}`,JSON.stringify(body),options)
+    //return this.httpClient.post(url,body, options);
 
   }
 
