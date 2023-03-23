edb34f0b98e948e7276e541e41aef64aaa09aead
sara-ghazy <saraghazy434@gmail.com>
2023-03-23 14:25:51 +0200

    edit

--- src/app/administration/admin-authors/admin-authors.component.ts
+++ src/app/administration/admin-authors/admin-authors.component.ts
@@ -76,7 +76,7 @@ export class AdminAuthorsComponent implements OnInit {
 
 
 
-  deleteAuthor(id: Number) {
+  deleteAuthor(id: string) {
     Swal.fire({
       title: 'Are you sure?',
       text: "You won't be able to revert this!",
