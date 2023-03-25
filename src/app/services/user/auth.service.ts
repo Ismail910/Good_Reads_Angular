c8f5fc35280c21b5e777cbfa41bd29cbe45f4d7a
IbrahimSalahEldin <ibrahimsalah824@gmail.com>
2023-03-25 19:00:35 +0200

    add logout and updates header

--- src/app/services/user/auth.service.ts
+++ src/app/services/user/auth.service.ts
@@ -4,17 +4,19 @@ import {HttpClient} from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
 import { environment } from 'src/environments/environment';
 import jwtDecode from 'jwt-decode'
+import { Router } from '@angular/router';
+Router
 @Injectable({
   providedIn: 'root'
 
 })
 export class AuthService {
   currentUser= new BehaviorSubject(null) 
-  constructor(private _HttpClient:HttpClient) {
+  constructor(private _HttpClient:HttpClient,private _Router:Router) {
 
-  //   if(localStorage.getItem('token') != null) {
-  //     // this.savecurrentuser();
-  // }
+    if(localStorage.getItem('token') != null) {
+      this.saveCurrentUser();
+  }
   }
 
   register(data:any): Observable<any>{
@@ -23,7 +25,6 @@ export class AuthService {
   login(data:any): Observable<any>{
     return this._HttpClient.post(`${environment.baseUrl}/login`,data)
   }
-
 
   saveCurrentUser()
   {
@@ -36,4 +37,11 @@ export class AuthService {
   {
     return this.currentUser.asObservable();
   }
+  logout(){
+    this.currentUser.next(null)
+    localStorage.removeItem('token')
+    localStorage.removeItem('isLogin')
+    this._Router.navigate(['/login']);
+  }
 }
+
