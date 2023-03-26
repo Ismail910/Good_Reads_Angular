aecc5ef637a4af30751da97b0dec9ae3279fa7b6
sara-ghazy <saraghazy434@gmail.com>
2023-03-26 14:03:17 +0200

    dashboard

--- src/app/administration/admin-panel/admin-panel.component.ts
+++ src/app/administration/admin-panel/admin-panel.component.ts
@@ -1,21 +1,30 @@
 import { NavigationEnd, Router } from '@angular/router';
-import { Component, OnChanges, SimpleChanges, OnInit } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
   styleUrls: ['./admin-panel.component.css']
 })
-export class AdminPanelComponent implements OnInit{
+export class AdminPanelComponent {
+  @Output() toggle:boolean=false;
+  namePage:String=this.router.url;
+
   constructor(private router:Router){
+    router.events.subscribe((val) => {
+      if(val instanceof NavigationEnd){
+         console.log(val.url);
+         this.namePage=(val.url.split('/'))[2];
+      }
+  });
+  }
+  
+  sidbar(status:boolean)
+  {
+    this.toggle=status;
+  }
 
-  }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-  ngOnChanges(): void {
 
-  }
 
 
 
