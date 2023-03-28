c03bdccbad74f75faa333548a2da0e7590cdaaf7
Ismail910 <ismailmagdy566@gmail.com>
2023-03-28 05:50:48 +0200

    update

--- src/app/users/components/user-books/user-books.component.ts
+++ src/app/users/components/user-books/user-books.component.ts
@@ -58,14 +58,14 @@ export class UserBooksComponent implements OnChanges,OnInit{
   getBooks()
     {
       console.log("asd");
-      this.Api.get(`${environment.baseUrl}/home/all/page/1/${this.userId}`).subscribe(data=>{
+      this.Api.get(`${environment.baseUrl}/home/all/page/${this.page}/${this.userId}`).subscribe(data=>{
         this.books=data.data;
         console.log(this.books);
         console.log(data);
         this.totalPages=data.pages.totalPages;
         this._pagination=[...Array(this.totalPages).keys()];
         console.log(this.userId);
-      })ad
+      })
     }
 
     getBooksByStatus(status:string)
