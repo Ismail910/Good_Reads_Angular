2439edd8c55990a85d11d97feaf634301f3a5f25
Ismail910 <ismailmagdy566@gmail.com>
2023-03-28 12:00:07 +0200

    mage

--- src/app/users/components/user-category-details/user-category-details.component.ts
+++ src/app/users/components/user-category-details/user-category-details.component.ts
@@ -1,54 +1,49 @@
-import { Component } from '@angular/core';
+
+import { ApiService } from './../../../@core/api.service';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/services/user/category.service';
 import { Book } from 'src/app/@shared/model/book';
-
+import { AuthService } from 'src/app/services/user/auth.service';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-user-category-details',
   templateUrl: './user-category-details.component.html',
   styleUrls: ['./user-category-details.component.css'],
 })
-export class UserCategoryDetailsComponent {
-  category!: any;
-  categoryID!: number;
-  books!: Book[];
-  categories!:any
+export class UserCategoryDetailsComponent implements OnInit{
 
+  categoryID!: string;
+  books!: Book[];
+  categories?:any
+  userData?:any
+  userId?:any
   constructor(
     private categoryService: CategoryService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private Api:ApiService,
+    private auth:AuthService
   ) {
-    // this.activatedRoute.paramMap.subscribe((paramMap) => {
-    //   //convert ID to int and check to null
-    //   // this.categoryID =  +paramMap.get('id')! || 1  ;
-    //   this.categoryService
-    //     .getCategory(paramMap.get('id'))
-    //     .subscribe((category) => {
-    //       this.category = category;
-    //     });
-    // });
-//To get category name 
-    this.categoryService.getCategories(1).subscribe((aut) => {
-      this.categories = aut.data;
-      console.log(this.categories );  
-    }  );
-//To get category books
-    this.activatedRoute.paramMap.subscribe((paramMap) => {
-      var ID=paramMap.get('id')
-      // console.log(ID);
-      this.categoryService
-        .getcategorybooks(ID)
-        .subscribe((books) => {
-          this.books = books;
-          console.log(this.books);
-        });
+    this.auth.getuser().subscribe((user: any)=>{
+      this.userData = user;
+      this.userId = this.userData.user._id
+     })
+
+    this.activatedRoute.paramMap.subscribe((paramMap)=>{
+      this.categoryID = paramMap.get('id') || ""
     });
   }
+  ngOnInit(): void {
+    this.getcategory()
+  }
+
+getcategory(){
+  this.Api.get(`${environment.baseUrl}/category/${this.categoryID}/${this.userId}`).subscribe(data=>{
+    this.categories = data
+    console.log(this.categories);
+  })
 }
 
 
-// this.activateRoute.paramMap.subscribe((paramMap)=>{
-//   this.currentProdId=Number(paramMap.get('id'));
-//   this.prod=this.staticProdService.getProductById(this.currentProdId);
 
-// });
+}
